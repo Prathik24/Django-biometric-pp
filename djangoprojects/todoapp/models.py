91b3bd8313b7e65ee458365a1339a97404ceb271
Prathik24 <prathikprasad98@gmail.com>
2022-02-27 09:35:09 -0700

    Updated version

--- djangoprojects/todoapp/models.py
+++ djangoprojects/todoapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class todo(models.Model):
-    objects = None
+
     title = models.CharField(max_length=255)
     description = models.TextField()
     priority = models.IntegerField()
